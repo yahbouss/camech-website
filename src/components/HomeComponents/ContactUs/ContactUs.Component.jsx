d67322d62d989a30d7c77bcb7555f343b386fff4
yahbouss <47297430+yahbouss@users.noreply.github.com>
2021-04-18 20:14:56 +0100

    need to fix footer

--- src/components/HomeComponents/ContactUs/ContactUs.Component.jsx
+++ src/components/HomeComponents/ContactUs/ContactUs.Component.jsx
@@ -1,10 +1,56 @@
-import { Container } from 'react-bootstrap'
+import { Container, Form, Col } from 'react-bootstrap'
+import { useState } from 'react'
 import './contactUs.scss'
 
 const ContactUs = () =>{
+    const [Name, setName] = useState('')
+    const [Email, setEmail] = useState('')
+    const [Message, setMessage] = useState('')
+
+    const submitForm = () =>{
+        console.log(Name + ' ' + Email + ' ' + Message)
+    }
+
     return (
-        <Container fluid='xl'>
-            <h1>contact us</h1>
+        <Container fluid='xl' className='form'>
+            <Form>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control 
+                    value={Name} 
+                    type="text" 
+                    placeholder="John Doe" 
+                    onChange={e => setName(e.target.value)}
+                    />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                    value={Email} 
+                    type="email" 
+                    placeholder="Password"
+                    onChange={e => setEmail(e.target.value)} 
+                    />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Group controlId="formGridAddress1">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control 
+                    value={Message} 
+                    className='form__message' 
+                    as="textarea" 
+                    placeholder="Your message here..." 
+                    onChange={e => setMessage(e.target.value)}
+                    />
+                </Form.Group>
+            </Form>
+            <button className='btn-submit' onClick={submitForm}>Submit</button>
         </Container>
     )
 }
